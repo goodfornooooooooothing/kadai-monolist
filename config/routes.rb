633d8f19e1b7eae1224178e0c38c8fbcf3075db0
EC2 Default User <goodfornooooooooothing@gmail.com>
2018-04-25 01:21:14 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
 
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
@@ -10,4 +13,6 @@ Rails.application.routes.draw do
 
   resources :items, only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
+  
+
 end
